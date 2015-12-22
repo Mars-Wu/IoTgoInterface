8e141e7ad9fea5009e14e7eeade4b1ee8a3f0e78
shennongmin <wupangfee@gmail.com>
2015-12-22 15:40:10 +0800

    Changed main.c

--- main.c
+++ main.c
@@ -7,9 +7,17 @@ const StdStream *ota_svr_stream = NULL;
 
 void main(void)
 {
+    uint8 buf[16];
+    
     ...
+    
     pair_stream = strmGetPairStream();
-
+    pair_stream->open();
+    pair_stream->state();
+    pair_stream->write(buf, sizeof(buf));
+    pair_stream->read(buf, pair_stream->available());
+    pair_stream->close();
+    
     ...
     
     boot_svr_stream = strmGetBootSvrStream(boot_ip, boot_port);
@@ -20,6 +28,11 @@ void main(void)
 
     ...
 
+    opr_svr_stream->close();
+    opr_svr_stream = strmGetOprSvrStream(opr2_ip, opr2_port);
+
+    ...
+    
     ota_svr_stream = strmGetOtaSvrStream(ota_ip, ota_port);
 
     ...
