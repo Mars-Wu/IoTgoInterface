0ed814d90100b8e9237acd0f8ff01d25ccffa2bf
shennongmin <wupangfee@gmail.com>
2015-12-30 09:29:31 +0800

    Signed-off-by: shennongmin <wupangfee@gmail.com>

---
+++ wormhole.h
@@ -0,0 +1,11 @@
+/**
+ * @file wormhole.h
+ * @brief The definition of Wormhole Interface based on IoTgo Protocol. 
+ */
+#ifndef __WORMHOLE_H__
+#define __WORMHOLE_H__
+
+
+
+#endif /* #ifndef __WORMHOLE_H__ */
+
